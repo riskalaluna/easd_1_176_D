b059f3c6d800c9f103bca9c49025daa5f8996e22
riskalaluna <riskalaluna@gmail.com>
2023-04-07 10:14:01 +0700

    Add project files.

---
+++ easd_1_176_D/easd_1_176_D.cpp
@@ -0,0 +1,9 @@
+//1. karena Algoritma mampu mempermudah dalam memecahkan permasalahan yang berkaitan dengan suatu program yang tidak dapat diselesaikan dengan mudah, serta algoritma mampu menyederhhanakan sebuah program;
+//2. Statis dan dinamis.
+//3. faktor-faktor yang mempengaruhi efisiensi program algoritma: Ada output, efektifitas dan efesiensi, jumlah langkahnya berhingga, (semi algoritma) berakhir dan terstruktur.
+//4. Algoritma quicksort, karena kinerja algoritma ini sangat cepat dibandingkan algoritma pengurutan data lainnya. Quicksort memanfaatkan metode drive and conquer yang memungkinkan untuk mengurangi kompleksitas waktu secara drastis.
+//5. Quadratic: Bubbleshort, Selectionsort, Insertionsort dan Shellsort. Loglinear: Quicksort dan Margesort.
+
+#include <iostream>
+using namespace std;
+
